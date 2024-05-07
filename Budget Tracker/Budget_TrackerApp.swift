4d16aafc1bff3c65632dd751efe07e2cf9f2a7f5
matt54633 <matt54633@gmail.com>
2024-05-07 15:44:26 +0100

    Initial Commit

---
+++ Budget Tracker/Budget_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_TrackerApp.swift
+//  Budget Tracker
+//
+//  Created by Matt Sullivan on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Budget_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
